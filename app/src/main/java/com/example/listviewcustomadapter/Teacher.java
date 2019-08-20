c09a192ceb837f1e5884dd7254a02e67bdcff885
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-08-20 17:09:08 +0530

    Update Teacher.java

--- app/src/main/java/com/example/listviewcustomadapter/Teacher.java
+++ app/src/main/java/com/example/listviewcustomadapter/Teacher.java
@@ -6,13 +6,15 @@ import java.util.ArrayList;
 //POJO = Plain Old Java Objects
 
 public class Teacher {
-String name;
-String course;
+    String name;
+    String course;
+
     //Constructor using Alt+Insert
     public Teacher(String name, String course) {
         this.name = name;
         this.course = course;
     }
+
     //Getter using Alt+Insert
     public String getName() {
         return name;
@@ -22,17 +24,17 @@ String course;
         return course;
     }
 
-    public static ArrayList<Teacher> get8RandomTeachers(){
+    public static ArrayList<Teacher> get8RandomTeachers() {
         //We now creater a new ArrayList and add the data
         ArrayList<Teacher> teachers = new ArrayList<>();
-        teachers.add(new Teacher("Jager","ADS"));
-        teachers.add(new Teacher("Rook","Armour"));
-        teachers.add(new Teacher("Thermite","Exothermic Charge"));
-        teachers.add(new Teacher("Kapkan","EDD"));
-        teachers.add(new Teacher("Sledge","Hammer"));
-        teachers.add(new Teacher("Ash","Rocket Launcher"));
-        teachers.add(new Teacher("Blitz","Flash Shield"));
-        teachers.add(new Teacher("Doc","Stim Pistol"));
+        teachers.add(new Teacher("Jager", "ADS"));
+        teachers.add(new Teacher("Rook", "Armour"));
+        teachers.add(new Teacher("Thermite", "Exothermic Charge"));
+        teachers.add(new Teacher("Kapkan", "EDD"));
+        teachers.add(new Teacher("Sledge", "Hammer"));
+        teachers.add(new Teacher("Ash", "Rocket Launcher"));
+        teachers.add(new Teacher("Blitz", "Flash Shield"));
+        teachers.add(new Teacher("Doc", "Stim Pistol"));
 
         return teachers; // Here we return the ArrayList !
 
